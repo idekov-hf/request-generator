b41d13c8e9a201753ba4e7d97c16b591cc115752
root <root@omarmorales-goobuntu.aus.corp.google.com>
2018-04-23 10:21:50 -0500

    add exponential backoff to avoid QPS spikes

--- index.js
+++ index.js
@@ -1,48 +1,68 @@
 const fetch = require('node-fetch');
 const fs = require('fs');
 const prompt = require('prompt');
+const startTime = new Date().toString();
 
-function repeatRequest(url, numRequests) {
+function backoffBrowser(url, numRequests, projId, timeout) {
+  makeRequest(url, 0);
+
+  const delay = (timeout * 1000);
+  const details = `URL: ${url}\n
+        Start Time: ${startTime}\n
+        Delay between requests in second(s): ${timeout}\n
+        Request(s) Made: ${numRequests}\n
+        End Time: ${new Date().toString()}\n`;
 
   function makeRequest(url, currentIteration) {
     if (currentIteration === numRequests) {
-      logTestDetails(url, numRequests);
-      return;
+      logmyTest();   
+      console.log('Check data-object-details.txt for the deets of this test :) \n\n');  
+      } else {
+      emitFetch(url, currentIteration);
+      }
     }
 
+  function emitFetch(url, currentIteration) {
     fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
+    .then( response => response.json())
+    .then( data => {
       console.log(data);
-      makeRequest(url, ++currentIteration);
-    });
-  }
+      setTimeout(() => makeRequest(url, ++currentIteration), delay);})
+    }
 
-  makeRequest(url, 0);
-}
+  function logmyTest() {
+    const myParse = `Test for project ${projId}  --\n  ${details} \n\n `;
 
-function logTestDetails(url, numRequests) {
-  const details = `url: ${url}
-end time: ${new Date().toString()}
-requests made: ${numRequests}\n\n`;
-  fs.appendFile('test-details.txt', details, (err) => {
-    if (err) throw err;
-    console.log('Check test-details.txt for details of this test case.');
-  });
-}
+    fs.appendFile('data-object-details.txt', myParse, (err) => {       
+            if (err) throw err;})
+    }
+ }
 
 prompt.start();
 prompt.get([{
-    description: 'URL',
-    name: 'url',
-    type: 'string',
-    required: true,
-  }, {
-    description: 'Number of requests',
-    name: 'requests',
-    type: 'integer',
-    message: 'Please enter a valid integer.',
-    required: true,
-  }], (err, result) => {
-    repeatRequest(result.url, Number(result.requests));
-});
+  description: 'URL:',
+  name: 'url',
+  type: 'string',
+  required: true,
+}, {
+  description: 'Number of requests',
+  name: 'requests',
+  type: 'integer',
+  message: 'Please enter a valid integer for the total amount of attempts:',
+  required: true,
+}, {
+  description: 'Spice-Project Details',
+  name: 'projectId',
+  type: 'string',
+  message: 'Please enter a ProjectID:',
+  required: true,
+}, {
+  description: 'Delay',
+  name: 'delay',
+  type: 'integer',
+  message: 'Delay in second(s)',
+  required: true
+
+}], (err, result) => {
+    backoffBrowser(result.url, Number(result.requests), result.projectId, Number(result.delay));
+    });
