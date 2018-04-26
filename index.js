8878d1afbe1edf385ed48d9b920cae0f9d0a0009
root <root@omarmorales-goobuntu.aus.corp.google.com>
2018-04-26 12:39:39 -0500

    fix spelling of response

--- index.js
+++ index.js
@@ -28,12 +28,12 @@ function backoffBrowser(url, numRequests, projId, timeout) {
         this is here for streetview testing as you cannot JSON an image response
         instead we store it by using:
         
-        .then ( respone => response.json())  > .then( response => images.push(response)) 
+        .then ( response => response.json())  > .then( response => images.push(response)) 
 
        **/ 
     var images = [];
     fetch(url)
-    .then( response => images.push(response))
+    .then( response => response.json()) 
     .then( data => {
       console.log(data);
       setTimeout(() => makeRequest(url, ++currentIteration), delay);})
